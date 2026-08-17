3f76121282dac091dc7986ff4b096a40ae0e06d3
Kumkum Yadav <yadavkumkum9009@gmail.com>
2026-08-17 22:51:54 +0530

    Initial setup

---
+++ backend/app/models/user.py
@@ -0,0 +1,16 @@
+from os import getenv
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+load_dotenv()
+
+DATABASE_URL = getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
